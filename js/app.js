8e24c53c64bccc57b0deed9c80ad16003f980e67
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2023-01-23 09:46:59 +0600

    removed => proxyUrl

--- js/app.js
+++ js/app.js
@@ -25,7 +25,7 @@ async function getQuote() {
     const proxyUrl = 'https://whispering-tor-04671.herokuapp.com/'
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
-        const response = await fetch(proxyUrl + apiUrl);
+        const response = await fetch(apiUrl);
         const data = await response.json();
         // If Author is blank, add 'Unknown'
         if (data.quoteAuthor === '') {
